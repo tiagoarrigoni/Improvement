598db3498225c292d75fe0371f54b8f96e33d190
Tiago Andrade Arrigoni <tiagoarrigoni@HEC.LOCAL>
2025-08-05 10:01:09 -0300

    Class 200,201

---
+++ Udemy_Cod3r/src/modelo9/streams/DesafioMap.java
@@ -0,0 +1,52 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+
+public class DesafioMap {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * 1. Número para string binária... 6 => "110"
+		 * 2. Inverter a string "110" => "011"
+		 * 3. Converter de volta para inteiro => "011" => 03
+		 */
+		
+		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9);
+		
+		UnaryOperator<String> inversor =
+				s -> new StringBuilder(s).reverse().toString();
+		
+		Function<String, Integer> conversor =
+				i -> Integer.parseInt(i, 2);
+		
+		nums.stream()
+		.map(Integer::toBinaryString)
+		.map(inversor)
+		.map(conversor)
+		.forEach(System.out::println);
+		
+		
+		
+		//List<String> numsBinario = nums.stream()
+				//.map(Integer::toBinaryString)
+				//.collect(Collectors.toList());
+		
+		//List<String> numsBinarioInvertido = numsBinario.stream()
+				//.map(s -> new StringBuilder(s).reverse().toString())
+				//.collect(Collectors.toList());
+		
+		//List<Integer> conversorNumsInvertido = numsBinarioInvertido.stream()
+				//.map(i-> Integer.parseInt(i, 2))
+				//.collect(Collectors.toList());
+		
+		//System.out.println(conversorNumsInvertido);
+		
+		
+	}
+
+}
